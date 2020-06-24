57070d268d51e6d1c6dc25e9e401e76e976473d5
Sabra Snider <sabralsnider@gmail.com>
2020-06-24 09:53:49 -0400

    Add project files.

---
+++ TwelveNote.WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(TwelveNote.WebMVC.Startup))]
+namespace TwelveNote.WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
