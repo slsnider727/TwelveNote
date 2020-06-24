417cc1ffb47dd7757c4781cc5ac5d0dd1448a3e3
Sabra Snider <sabralsnider@gmail.com>
2020-06-24 11:28:38 -0400

    Add NoteListItem, Note Controller Index, Note Index View

---
+++ TwelveNote.WebMVC/Controllers/NoteController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TwelveNote.Models;
+
+namespace TwelveNote.WebMVC.Controllers
+{
+    [Authorize]
+    public class NoteController : Controller
+    {
+        // GET: Note
+        public ActionResult Index()
+        {
+            var model = new NoteListItem[0];
+            return View(model);
+        }
+    }
+}
